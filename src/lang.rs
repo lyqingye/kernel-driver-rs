2286cc8bf07f1e5abf589a516453e3592e17f17c
lyqingye <>
2023-02-13 23:00:08 +0800

    first commit

---
+++ src/lang.rs
@@ -0,0 +1,26 @@
+use core::panic::PanicInfo;
+use kernel_alloc::KernelAlloc;
+
+#[link(name = "ntoskrnl")]
+extern "system" {
+    pub fn KeBugCheck(bug_check_code: u32) -> !;
+}
+
+#[no_mangle]
+#[allow(bad_style)]
+static _fltused: i32 = 0;
+
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
+
+#[no_mangle]
+extern "C" fn __CxxFrameHandler3() {}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo<'_>) -> ! {
+    log::info!("Panic handler called: {:?}", _info);
+    unsafe { KeBugCheck(0x000000E2) };
+}
+
+#[global_allocator]
+static GLOBAL: KernelAlloc = KernelAlloc;
---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+#![no_std]
+#![feature(lang_items)]
+#![feature(decl_macro)]
+#![feature(box_syntax)]
+#![feature(new_uninit)]
+#![feature(link_llvm_intrinsics)]
+#![allow(clippy::new_ret_no_self)]
+#![allow(clippy::not_unsafe_ptr_arg_deref)]
+
+use kernel_log::KernelLogger;
+use log::LevelFilter;
+use winapi::{
+    km::wdm::DRIVER_OBJECT,
+    shared::{
+        ntdef::{NTSTATUS, PVOID},
+        ntstatus::STATUS_SUCCESS,
+    },
+};
+
+pub mod lang;
+pub mod nt;
+
+pub extern "system" fn driver_unload(_driver: &mut DRIVER_OBJECT) {
+    log::info!("Call Driver Unload");
+}
+
+#[no_mangle]
+pub extern "system" fn DriverEntry(driver: *mut DRIVER_OBJECT, _path: PVOID) -> NTSTATUS {
+    KernelLogger::init(LevelFilter::Info).unwrap();
+    log::info!("Call Driver Entry");
+    unsafe { (*driver).DriverUnload = Some(driver_unload) };
+    STATUS_SUCCESS
+}
